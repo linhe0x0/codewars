b7eff97f4503fd04421a223048448331397363f0
sqrtthree <imsqrtthree@gmail.com>
2018-01-24 20:12:49 +0800

    chore: :rocket: 删除部署到 coding pages 的脚本

--- deploy.js
+++ deploy.js
@@ -9,13 +9,3 @@ ghpages.publish('_book', {
   console.log('Deploy to GitHub successfully.')
   console.log('To http://blog.sqrtthree.com/codewars to preview.')
 })
-
-ghpages.publish('_book', {
-  branch: 'coding-pages',
-  repo: 'git@git.coding.net:sqrtthree/codewars.git',
-  message: `Update at ${new Date().toISOString()}`,
-}, () => {
-  console.log()
-  console.log('Deploy to Coding successfully.')
-  console.log('To http://sqrtthree.coding.me/codewars to preview.')
-})
