53eef4acfa7cf9c482934c2d449dd204509aa780
sqrtthree <imsqrtthree@gmail.com>
2022-04-17 00:46:51 +0800

    fix: set base path

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const withNextra = nextra('nextra-theme-docs', './theme.config.js')
 
 module.exports = withNextra({
+  basePath: isProd ? '/codewars' : '',
   assetPrefix: isProd ? '/codewars/' : '',
   pageExtensions: ['mdx', 'md', 'jsx', 'page.js'],
 })
